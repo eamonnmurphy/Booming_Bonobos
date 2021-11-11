016209cb49ba6d478c0ca2aea91d70966712d42f
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2021-11-11 11:53:44 +0000

    Completed second alignment gw practical

--- week2/code/align_seqs_better.py
+++ week2/code/align_seqs_better.py
@@ -9,10 +9,11 @@ __version__ = '0.0.1'
 ## import
 
 import sys
+import pickle
 
 # Two example sequences to match
-#seq2 = "ATCGCCGGATTACGGG"
-#seq1 = "CAATTCGGAT"
+seq2 = "ATCGCCGGATTACGGG"
+seq1 = "CAATTCGGAT"
 
 ## import and prepare the input sequences
 def ReadInput(x): 
@@ -35,13 +36,44 @@ def calculate_score(s1, s2, l1, l2, startpoint): # it will run with the startpoi
             else:
                 matched = matched + "-"
     # some formatted output
-    print("." * startpoint + matched) # "." * startpoint means how many time "*" is repeated based on the number of startpoint          
-    print("." * startpoint + s2)
-    print(s1)
-    print(score) 
-    print(" ")
 
     return score
+
+def find_best_seqs(seqA = seq1, seqB = seq2):
+    # Assign the longer sequence s1, and the shorter to s2
+    # l1 is length of the longest, l2 that of the shortest
+    l1 = len(seqA)
+    l2 = len(seqB)
+    if l1 >= l2:
+        s1 = seqA
+        s2 = seqB
+    else:
+        s1 = seqB
+        s2 = seqA
+        l1, l2 = l2, l1 # swap the two lengths
+
+    #now try to find the best match (highest score) for the two sequences
+    my_best_align = None
+    my_best_score = -1
+    all_best = {}
+
+    for i in range(l1): # Note that you just take the last alignment with the highest score
+        z = calculate_score(s1, s2, l1, l2, i)
+        if z > my_best_score:
+            all_best = {}
+
+            my_best_align = "." * i + s2 # think about what this is doing!
+            my_best_score = z
+
+            all_best["Align " + str(1)] = [my_best_score, my_best_align]
+        elif z == my_best_score:
+            my_best_align = "." * i + s2 # think about what this is doing!
+            
+            value = len(all_best.keys())
+
+            all_best["Align " + str(value + 1)] = [my_best_score, my_best_align]
+    
+    return(all_best)
 
 def main(argv):
     if len(sys.argv) <= 2 : # if there is no input argument or only one argument
@@ -64,35 +96,12 @@ def main(argv):
         #Read input files and define seq1 and seq2
         seq1, seq2 = ReadInput(sys.argv[1]), ReadInput(sys.argv[2])
 
+    my_best_scores = find_best_seqs()
     
+    with open("../results/seq_aligns.txt", "w") as f:
+        for key, value in my_best_scores.items():
+            f.write("%s: %s\n" % (key, value))
 
-# Assign the longer sequence s1, and the shorter to s2
-# l1 is length of the longest, l2 that of the shortest
-    l1 = len(seq1)
-    l2 = len(seq2)
-    if l1 >= l2:
-        s1 = seq1
-        s2 = seq2
-    else:
-        s1 = seq2
-        s2 = seq1
-        l1, l2 = l2, l1 # swap the two lengths
-
-
-
-#now try to find the best match (highest score) for the two sequences
-    my_best_align = None
-    my_best_score = -1
-
-    for i in range(l1): # Note that you just take the last alignment with the highest score
-        z = calculate_score(s1, s2, l1, l2, i)
-        if z > my_best_score:
-            my_best_align = "." * i + s2 # think about what this is doing!
-            my_best_score = z 
-    print(my_best_align)
-    print(s1)
-    print("Best score:", my_best_score)
-    print("Done!")
     return None
 
 if (__name__ == "__main__"):
