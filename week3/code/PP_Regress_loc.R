75f0e65ae418d19f520df285e441c3da52886cbe
emma <emma.macdonald21@imperial.ac.uk>
2022-01-09 13:18:57 +0000

    pp regress file

---
+++ week3/code/PP_Regress_loc.R
@@ -0,0 +1,40 @@
+############################## Regression Practical #############################
+
+# load the data
+mydata <- read.csv("../data/EcolArchives-E089-51-D1.csv")
+
+# load package
+require(plyr)
+
+# Notes to self:
+#dlply applies function to groups of data frame -> makes list
+#ldply -> takes list, makes data frame
+
+# stores linear regressions for each feeding interaction, location and predator lifestage in model 1
+lmfunction <- function(mydata){
+  summary(lm(Prey.mass ~ Predator.mass, data = mydata))
+}
+model1 <- dlply(mydata, as.quoted(.(Type.of.feeding.interaction, Predator.lifestage, Location)), lmfunction)
+
+# gets separate stats out of model1
+listfunction <- function(m){ 
+  slope <- m$coefficients[2]
+  intercept <- m$coefficients[1]
+  r2 <- m$r.squared
+  pvalue <- m$coefficients[8]
+  data.frame(slope, intercept, r2, pvalue)
+}
+output <- ldply(model1, listfunction)
+
+# f stat is weird, get out of model1 separately:
+listfunction2 <- function(m){
+  fstat <- m$fstatistic[1]
+  data.frame(fstat)
+}
+output2 <- ldply(model1, listfunction2)
+
+# merge data frames: f stat and the rest of stats
+finaloutput <- merge(output, output2)
+
+# write a csv file
+write.csv(finaloutput, "../results/PP_Regress_loc_Results.csv", row.names = FALSE) # gets rid of row names 1, 2, 3
