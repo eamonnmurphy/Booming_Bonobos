c87190074a8eb102b132c83d3e660242bf6561ae
emma <emma.macdonald21@imperial.ac.uk>
2022-01-15 13:03:02 +0000

    bash script

---
+++ week3/code/run_get_TreeHeight.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Emma MacDonald em221@ic.ac.uk
+# Script: run_get_TreeHeight.sh
+# Description: tests get_TreeHeight.R and get_TreeHeight.py
+# Arguments: 1 argument
+# Date: Jan 2022
+
+if [ -f "$1" ]; then # checks if file exists
+# if file exists:
+
+echo "Running get_TreeHeight.R"
+Rscript get_TreeHeight.R $1
+
+echo "Running get_TreeHeight.py"
+python3 get_TreeHeight.py $1
+
+else # if file doesn't exist, run on example file:
+
+echo "Running scripts on example file ../data/trees.csv"
+
+echo "Running get_TreeHeight.R"
+Rscript get_TreeHeight.R ../data/trees.csv
+
+echo "Running get_TreeHeight.py"
+python3 get_TreeHeight.py ../data/trees.csv
+
+fi # close if else statements
